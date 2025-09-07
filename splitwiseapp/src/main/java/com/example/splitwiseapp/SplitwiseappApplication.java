8da6ec8609c05c7762158aa541df4bef7b7551c5
Surya <rvs.surya123@gmail.com>
2025-09-07 12:33:30 +0530

    Initial commit

---
+++ splitwiseapp/src/main/java/com/example/splitwiseapp/SplitwiseappApplication.java
@@ -0,0 +1,13 @@
+package com.example.splitwiseapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitwiseappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitwiseappApplication.class, args);
+	}
+
+}
